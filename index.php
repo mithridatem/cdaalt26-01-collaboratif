7048e1059e590a09497994e54d1cb20a3b6e5869
3ddyCa <eddypennbass@proton.me>
2026-04-13 15:35:40 +0200

    feat:changmenbt de suffix d'index et ajout d'affichage

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+	  echo "<strong>Bonjour </strong> ".$_SERVER["REMOTE_HOST"];
+
+	echo "<strong>Votre ip : </strong> ".$_SERVER["SERVER_NAME"];
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+    <main>
+        <h1>Welcome</h1>
+    </main>
+</body>
+</html>
